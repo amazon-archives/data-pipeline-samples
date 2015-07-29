abe8277b431b596ea992746bbf1bfe73ce659290
Marc Beitchman <beitchma@amazon.com>
2015-07-29 11:46:47 -0700

    Small fixes to summary commands and error messages

--- samples/RDStoRedshiftSqoop/setup/Utilities.py
+++ samples/RDStoRedshiftSqoop/setup/Utilities.py
@@ -7,5 +7,5 @@ def check_working_directory():
     if current_folder_name == 'RDStoRedshiftSqoop':
         os.chdir('setup')
     elif current_folder_name != 'setup':
-        print 'ERROR: please run the setup script from data-pipeline-samples/samples/sqoop/setup'
+        print 'ERROR: please run the setup script from data-pipeline-samples/samples/RDStoRedshiftSqoop/setup'
         sys.exit(0)
